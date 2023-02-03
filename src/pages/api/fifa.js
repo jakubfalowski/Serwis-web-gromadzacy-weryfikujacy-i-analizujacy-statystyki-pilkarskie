00227fdbdf8c689732aba09ddb6b909b4f4caa70
Jakub Fałowski <jakub.falowski@febrit.co>
2023-02-03 17:31:10 +0100

    fix: delete json

--- src/pages/api/fifa.js
+++ src/pages/api/fifa.js
@@ -1,6 +1,5 @@
 import Fifa from "./appfifa";
 import dictPlayers from "../../app/components/dictPlayers";
-import { Fifa21 } from "../../app/components/fifa21";
 
 import NextCors from 'nextjs-cors';
 
@@ -8,27 +7,27 @@ export default async function FifaAPI(req,res) {
   await NextCors(req, res, {
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
     origin: '*',
-    optionsSuccessStatus: 200,
+    optionsSuccessStatus: 200
  });
 
-  res.json(Fifa21)
-    // const results = await Fifa();
+  const results = await Fifa();
 
-    // const interval = setInterval(timeToFetch, 1);
-    // function timeToFetch() {
-    //     if (results[0].length > 645){
-    //         clearInterval(interval);
-    //         const playerStats = results[0].map((row, index) => {
-    //             return results.map((attributes) => {
-    //               return attributes[index]
-    //             })
-    //           });
-    //           const jsonStats = playerStats.map((typeStat =>{
-    //             return {"name": dictPlayers(typeStat[0]), "rating": typeStat[1], "pace": typeStat[2], "shots":typeStat[3], "pass": typeStat[4], "dribble": typeStat[5], "defensive": typeStat[6], "physicality":typeStat[7]}
-    //           }));
-    //         res.json(jsonStats);
-    //     }
-      
-    // }
+  const interval = setInterval(timeToFetch, 1);
+  function timeToFetch() {
+      if (results[0].length > 645){
+          clearInterval(interval);
+          const playerStats = results[0].map((row, index) => {
+              return results.map((attributes) => {
+                return attributes[index]
+              })
+            });
+            const jsonStats = playerStats.map((typeStat =>{
+              return {"name": dictPlayers(typeStat[0]), "rating": typeStat[1], "pace": typeStat[2], "shots":typeStat[3], "pass": typeStat[4], "dribble": typeStat[5], "defensive": typeStat[6], "physicality":typeStat[7]}
+            }));
+          res.json(jsonStats);
+      }
+      else res.json("Error scrapping")
+    
+  }
 }
 
--- src/pages/api/fm.js
+++ src/pages/api/fm.js
@@ -1,5 +1,4 @@
 import Fm from "./appfm";
-import { Fm21 } from "../../app/components/fm21";
 
 import NextCors from 'nextjs-cors';
 
@@ -8,26 +7,25 @@ export default async function FmAPI(req,res) {
     // Options
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
     origin: '*',
-    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    optionsSuccessStatus: 200
  });
-    res.json(Fm21);
-    // const results = await Fm();
+  const results = await Fm();
 
-    // const interval = setInterval(timeToFetch, 1);
-    // function timeToFetch() {
-        // if (results[0].length >= 1080){
-        //     clearInterval(interval);
+  const interval = setInterval(timeToFetch, 1);
+  function timeToFetch() {
+      if (results[0].length >= 1080){
+          clearInterval(interval);
 
-        //     const playerStats = results[0].map((row, index) => {
-        //         return results.map((attributes) => {
-        //           return attributes[index]
-        //         })
-        //       });
+          const playerStats = results[0].map((row, index) => {
+              return results.map((attributes) => {
+                return attributes[index]
+              })
+            });
 
-        //       const jsonStats = playerStats.map((typeStat =>{
-        //          return {"name": typeStat[0], "rating": typeStat[1], "attack": typeStat[2], "defensive":typeStat[3], "technique": typeStat[4], "mentality": typeStat[5], "physicality": typeStat[6], "pace":typeStat[7], "club": typeStat[8], "nation": typeStat[9]}
-        //       }));
-        //     res.json(jsonStats);
-        // }
-    // }
+            const jsonStats = playerStats.map((typeStat =>{
+                return {"name": typeStat[0], "rating": typeStat[1], "attack": typeStat[2], "defensive":typeStat[3], "technique": typeStat[4], "mentality": typeStat[5], "physicality": typeStat[6], "pace":typeStat[7], "club": typeStat[8], "nation": typeStat[9]}
+            }));
+          res.json(jsonStats);
+      } else res.json("Error scrapping")
+  }
 }
